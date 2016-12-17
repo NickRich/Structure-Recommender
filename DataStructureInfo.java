ff5bcbb0c3a71deffa29b99a9e620a6ee3756eb9
Nicholas Richardson <richa271@purdue.edu>
2016-12-16 19:51:53 -0500

    Complexities
    Added most time complexities for access, insertion, search, and delete.

--- DataStructureInfo.java
+++ DataStructureInfo.java
@@ -3,16 +3,74 @@
  */
 public class DataStructureInfo {
     public void getInfo(String structure) {
+        System.out.println(structure);
         switch (structure) {
-            case "Tree": break;
-            case "Graph": break;
-            case "Heap": break;
-            case "Hash Table": break;
-            case "Trie": break;
-            case "Stack": break;
-            case "Queue": break;
-            case "Linked List": break;
-            case "Array/Matrix": break;
+            case "Tree":
+                System.out.println("Access: O(log(n))");
+                System.out.println("Search: O(log(n))");
+                System.out.println("Insertion: O(log(n))");
+                System.out.println("Deletion: O(log(n))");
+                break;
+            case "Graph":
+                System.out.println("Adjacency List:");
+                System.out.println("Access: O()");
+                System.out.println("Search: O()");
+                System.out.println("Insertion: O()");
+                System.out.println("Deletion: O()");
+                System.out.println("Adjacency Matrix:");
+                System.out.println("Access: O()");
+                System.out.println("Search: O()");
+                System.out.println("Insertion: O()");
+                System.out.println("Deletion: O()");
+                break;
+            case "Heap":
+                System.out.println("Access: O()");
+                System.out.println("Search: O()");
+                System.out.println("Insertion: O()");
+                System.out.println("Deletion: O()");
+                break;
+            case "Hash Table":
+                System.out.println("Access: O()");
+                System.out.println("Search: O(1)");
+                System.out.println("Insertion: O(1)");
+                System.out.println("Deletion: O(1)");
+                break;
+            case "Trie":
+                System.out.println("Access: O(log(n))");
+                System.out.println("Search: O(log(n))");
+                System.out.println("Insertion: O(log(n))");
+                System.out.println("Deletion: O(log(n))");
+                break;
+            case "Stack":
+                System.out.println("Access: O(n)");
+                System.out.println("Search: O(n)");
+                System.out.println("Insertion: O(1)");
+                System.out.println("Deletion: O(1)");
+                break;
+            case "Queue":
+                System.out.println("Access: O(n)");
+                System.out.println("Search: O(n)");
+                System.out.println("Insertion: O(1)");
+                System.out.println("Deletion: O(1)");
+                break;
+            case "Linked List":
+                System.out.println("Access: O(n)");
+                System.out.println("Search: O(n)");
+                System.out.println("Insertion: O(1)");
+                System.out.println("Deletion: O(1)");
+                break;
+            case "Array/Matrix":
+                System.out.println("Array:");
+                System.out.println("Access: O(1)");
+                System.out.println("Search: O(n)");
+                System.out.println("Insertion: O(n)");
+                System.out.println("Deletion: O(n)");
+                System.out.println("Matrix:");
+                System.out.println("Access: O(1)");
+                System.out.println("Search: O(n^2)");
+                System.out.println("Insertion: O(n^2)");
+                System.out.println("Deletion: O(n^2)");
+                break;
         }
     }
 }
